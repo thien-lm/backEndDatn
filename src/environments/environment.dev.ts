c44bb0061e16a88ff3923e2ca24ca18504edc95b
Le Minh Thien <100015056+thien-lm@users.noreply.github.com>
2024-11-29 22:22:23 +0700

    Update environment.dev.ts

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,7 +1,7 @@
 import { Environment } from "./environment";
 
 export const DevEnvironment: Environment = {
-  db_uri: "mongodb://localhost:27017/",
+  db_uri: "mongodb+srv://abcdef:111111111111a@cluster0.atwn6ip.mongodb.net/job-recommender-database",
   jwt_secret_key: "secretkey",
   jwt_refresh_secret_key: "rfscrerejwt_refresh_secret_key",
   sendgrid: {
